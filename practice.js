13851df8c283830ceb1fcff566d59860d324e40f
Adarsh KP <kpadarsh41@gmail.com>
2025-01-08 10:23:03 +0530

    second commit

--- practice.js
+++ practice.js
@@ -1,166 +1,155 @@
 //linked list
 
-class Node{
-    constructor(value){
-        this.value=value
-        this.next=null
-    }
-}
+// class Node{
+//     constructor(value){
+//         this.value=value
+//         this.next=null
+//     }
+// }
 
-class linkedList{
-    constructor(){
-        this.head=null
-        this.size=0
-    }
+// class linkedlist{
+//     constructor(){
+//         this.head=null
+//         this.size=0
+//     }
 
-    isEmpty(){
-        return this.size===0
-    }
+//     print(){
+//         if(this.size==0){
+//             return null
+//         }else{
+//         let curr=this.head
+//         let List=""
+//         while(curr){
+//             List+=`${curr.value} `
+//             curr=curr.next
+//         }
+//         console.log(List)
+//     }
 
-    prepend(value){
-        const node=new Node(value)
-        if(this.isEmpty()){
-            this.head=node
-        }else{
-            node.next=this.head
-            this.head=node
-        }
-        this.size++
-    }
+//     }
 
-    print(){
-        if(this.isEmpty()){
-            return null
-        }
-        else{
-            let current=this.head
-            let listvalue=""
-            while(current){
-                listvalue+=`${current.value} `
-                current=current.next
-            }
-            console.log(listvalue)
-        }
-    }
+//     prepend(value){
+//         const node=new Node(value)
+//         if(this.size==0){
+//             this.head=node
+//         }else{
+//             node.next=this.head
+//             this.head=node
+//         }
+//         this.size++
+//     }
 
-    append(value){
-        const node=new Node(value)
-        if(this.isEmpty()){
-            this.head=node
-        }
-        else{
-            let current=this.head
-            while(current.next){
-                current=current.next
-            }
-            current.next=node
-        }
-        this.size++
-    }
+//     append(value){
+//         const node=new Node(value)
+//         if(this.size==0){
+//             this.head=node
+//         }
+//         else{
+//             let curr=this.head
+//             while(curr.next!=null){
+//                 curr=curr.next
+//             }
+//             curr.next=node
+//         }
+//         this.size++
+//     }
 
-    insert(value,index){
-       if(index<0||index>this.size){
-            return null
-       }
-       if(index===0){
-        return this.prepend(value)
-       }
-       else{
-        const node=new Node(value)
-        let prev=this.head
-        for(let i=0;i<index-1;i++){
-            prev=prev.next
-        }
-        node.next=prev.next
-        prev.next=node
-       }
-       this.size++
-    }
+//     insert(index,value){
+//         if(index<0||index>this.size){
+//             return null
+//         }
+//         if(index===0){
+//             this.prepend(value)
+//         }
+//         else{
+//             const node=new Node(value)
+//             let prev=this.head
+//             for(let i=0;i<index-1;i++){
+//                 prev=prev.next
+//             }
+//             node.next=prev.next
+//             prev.next=node
+//         }
+//         this.size++
+//     }
 
-    remove(index){
-        if(index<0||index>this.size){
-            return null
-        }
-        let removedNode
-        if(index===0){
-        this.head=this.head.next
-        }
-    else{
-        let current=this.head
-        for(let i=0;i<index-1;i++){
-            current=current.next
-        }
-        removedNode=current.next
-        current.next=removedNode.next
-    }
-    this.size++
-    }
+//     remove(index){
+//         if(index<0||index>this.size){
+//             return null
+//         }
+//         let removedNode
+//         if(index==0){
+//             this.head=this.head.next
+//         }
+//         else{
+//             let prev=this.head
+//             for(let i=0;i<index-1;i++){
+//                 prev.prev.next
+//             }
+//             removedNode=prev.next
+//             prev.next=removedNode.next
+//         }
+//         this.size--
+//     }
 
-    removeValue(value){
-        if(this.isEmpty()){
-            return null
-        }
-        if(this.head.value===value){
-            this.head=this.head.next
-            return value
-        }
-        else{
-            let prev=this.head
-            while(prev.next&&prev.next.value!==value){
-                prev=prev.next
-            }
-            if(prev.next){
-            const removedNode=prev.next
-            prev.next=removedNode.next
-            this.size--
-            return value
-            }
-            return null
-        }
-    }
+//     search(value){
+//         if(this.size==0){
+//             return null
+//         }
+//         else{
+//             let curr=this.head
+//             let i=0
+//             while(curr){
+//                 if(curr.value==value){
+//                     return i
+//                 }
+//                 curr=curr.next
+//                     i++
+//             }
+//         }
+//         return null
+//     }
 
-    search(value){
-        if(this.isEmpty()){
-            return null
-        }
-        else{
-            let prev=this.head
-            let i=0
-            while(prev){
-                if(prev.value===value){
-                    return i
-                }
-                prev=prev.next
-                i++
-            }
-        }
-        return null
-    }
+//     removeValue(value){
+//         if(this.size==0){
+//             return null
+//         }
+//         else{
+//             let prev=this.head
+//             while(prev.next&&prev.next.value!==value){
+//                 prev=prev.next
+//             }
+//             if(prev.next){
+//                 const removedNode=prev.next
+//                 prev.next=removedNode.next
+//             }
+//         }
+//         return null
+//     }
 
-    reverse(){
-        let prev=null
-        let curr=this.head
-        let next
-        while(curr){
-            next=curr.next
-            curr.next=prev
-            prev=curr
-            curr=next
-        }
-        this.head=prev
-    }
+//     reverse()
+//     {
+//         let prev=null
+//         let curr=this.head
+//         let next
+//         while(curr){
+//             next=curr.next
+//             curr.next=prev
+//             prev=curr
+//             curr=next
+//         }
+//         this.head=prev
+//     }
+// }
 
-}
+// const list=new linkedlist()
 
-const list=new linkedList()
+// list.append(20)
+// list.prepend(10)
+// list.append(30)
+// list.reverse()
+// list.print()
 
-list.insert(10,0)
-list.insert(20,0)
-list.insert(30,1)
-list.print()
-
-list.reverse()
-list.print()
 
 
 
@@ -190,3 +179,47 @@ list.print()
 // }
 
 // console.log(fact(5))
+
+
+// function sum(a,b){
+//     let sum=0
+//     sum=a+b
+//     return sum
+// }
+// console.log(sum(10,20))
+
+
+// function fibonacci(n){
+//     let fib=[0,1]
+//     for(let i=2;i<n;i++)
+//     {
+//         fib[i]=fib[i-1]+fib[i-2]
+//     }
+//     return fib
+// }
+// console.log(fibonacci(5))
+
+
+//linear search
+
+// function search(arr,target){
+//     for(let i=0;i<arr.length;i++){
+//         if(target===arr[i]){
+//             return i
+//         }
+//     }
+//     return -1
+// }
+// let arr=[2,4,3,7,9,5,6,0]
+// console.log(search(arr,1))
+
+
+function fibonacci(n){
+    let fib=[0,1]
+    for(let i=2;i<n;i++){
+        fib[i]=fib[i-1]+fib[i-2]
+    }
+    return fib
+}
+
+console.log(fibonacci(6))
